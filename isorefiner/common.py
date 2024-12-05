28ec114f07f48cdd4aa96291bc28f5d08e36b959
rkajitani <rei.kajitani@gmail.com>
2024-12-05 17:33:50 +0900

    Update common.py

--- isorefiner/common.py
+++ isorefiner/common.py
@@ -5,6 +5,11 @@ import subprocess
 
 def run_command(cmd, out_file_name="", err_file_name=""):
     try:
+        base_cmd = cmd.split(" ")[0]
+        if out_file_name == "":
+            out_file_name = f"{base_cmd}.stdout"
+        if err_file_name == "":
+            err_file_name = f"{base_cmd}.stderr"
         with open(out_file_name, "w") as out_file, open(err_file_name, "a") as err_file:
             subprocess.run(
                 cmd,
