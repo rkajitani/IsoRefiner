106c50661077eb92a80562cfdea025daa602f067
rkajitani <rei.kajitani@gmail.com>
2024-12-06 15:40:46 +0900

    Add functions to filter files, and update logging-related codes

--- isorefiner/common.py
+++ isorefiner/common.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
+import logging
 import subprocess
 
 
-def run_command(cmd, out_file_name=None, err_file_name=None, logger=None):
+logger = logging.getLogger(__name__)
+
+
+def run_command(cmd, stdout=None, stderr=None):
     try:
         base_cmd = cmd.split(" ")[0]
-        if out_file_name is None:
-            out_file_name = f"{base_cmd}.stdout"
-        if err_file_name is None:
-            err_file_name = f"{base_cmd}.stderr"
-        if logger is not None:
-            logger.info(f"Running command: {cmd}, stdout: {out_file_name}, stderr: {err_file_name}")
-        with open(out_file_name, "w") as out_file, open(err_file_name, "a") as err_file:
+        if stdout is None:
+            stdout = f"{base_cmd}.stdout"
+        if stderr is None:
+            stderr = f"{base_cmd}.stderr"
+        logger.info(f"Running command: {cmd}, stdout: {stdout}, stderr: {stderr}")
+        with open(stdout, "w") as out_file, open(stderr, "a") as err_file:
             subprocess.run(
                 cmd,
                 shell=True,
@@ -21,7 +24,6 @@ def run_command(cmd, out_file_name=None, err_file_name=None, logger=None):
                 check=True,
                 text=True
             )
-        if logger is not None:
-            logger.info(f"Finished command: {cmd}")
+        logger.info(f"Finished command: {cmd}")
     except Exception as e:
         raise e
--- isorefiner/filter.py
+++ isorefiner/filter.py
@@ -4,12 +4,19 @@ import logging
 import os
 import subprocess
 import sys
+import polars as pl
+import pysam
 from isorefiner.common import run_command
 
+logger = logging.getLogger(__name__)
 
+        
 def main(args):
     try:
         # Set variables
+        raw_input_gtf = os.path.abspath(args.input_gtf)
+        raw_genome_fasta = os.path.abspath(args.genome_fasta)
+        raw_reads_fastq = os.path.abspath(args.reads_fastq)
         out_gtf = os.path.abspath(args.out_gtf)
         n_thread = args.threads
         max_indel = args.max_indel
@@ -23,6 +30,13 @@ def main(args):
         os.makedirs(work_dir, exist_ok=True)
         os.chdir(work_dir)
 
+        # Set logger
+        logging.basicConfig(
+            filename="log.txt",
+            level=logging.INFO,
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
         # Set input files and create symbolic links
         input_gtf = "raw.gtf"
         if os.path.lexists(input_gtf):
@@ -34,25 +48,60 @@ def main(args):
         reads_fastq = f"reads.{reads_ext}"
         if os.path.lexists(reads_fastq):
             reads_fastq = f"reads_{os.getpid()}.{reads_ext}"
-        os.symlink(os.path.abspath(args.input_gtf), input_gtf)
-        os.symlink(os.path.abspath(args.genome_fasta), genome_fasta)
-        os.symlink(os.path.abspath(args.reads_fastq), reads_fastq)
+        os.symlink(raw_input_gtf, input_gtf)
+        os.symlink(raw_genome_fasta, genome_fasta)
+        os.symlink(raw_reads_fastq, reads_fastq)
 
-        # Set logger
-        logging.basicConfig(
-            filename="log.txt",
-            level=logging.INFO,
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-        logger = logging.getLogger(__name__)
-
+        # Main process
         logger.info(f"Starting isorefiner filter (PID: {os.getpid()})")
-        run_command(cmd=f"echo 'gffread -w asm.fa -g genome.fa raw.gtf'", logger=logger)
+        run_command(f"gffread -w asm.fa -g {genome_fasta} {input_gtf}")
+        run_command(f"minimap2 -ax map-ont --secondary=no -t {n_thread} asm.fa {reads_fastq} | samtools view -b -F 2308 -", stdout="raw.bam")
+        run_command(f"samtools sort -@ {n_thread} -m 2G raw.bam", stdout="sorted.bam", stderr="samtools_sort.stderr")
+        filter_bam("sorted.bam", "filt.bam", max_indel, max_clip, min_idt)
+        run_command(f"samtools index filt.bam")
+        run_command(f"samtools coverage filt.bam", stdout="cov.tsv")
+        filter_coverage_table("cov.tsv", "cov_depth_filt.list", min_cov, min_mean_depth)
 
     except Exception as e:
-        print("Error:", e, file=sys.stderr)
-        print("Exception class:", type(e).__name__, file=sys.stderr)
+        msg = f"Error: {e}, Exception class: {type(e).__name__}"
+        print(msg, file=sys.stderr)
+        logger.error(msg)
         sys.exit(1)
+
+
+def filter_bam(in_bam, out_bam, max_indel, max_clip, min_idt):
+    log_func_str = f'filter_bam("{in_bam}", "{out_bam}", {max_indel}, {max_clip}, {min_idt})'
+    logger.info(f"Starting {log_func_str})")
+
+    with pysam.AlignmentFile(in_bam, "rb") as fin, pysam.AlignmentFile(out_bam, "wb", header=fin.header) as fout:
+        for aln in fin:
+            valid_flag = True
+            idt = 1.0 - (aln.get_tag("NM") / aln.query_alignment_length)
+            if idt < min_idt:
+                continue
+            for cig_ope, cig_len in aln.cigartuples:
+                if (((cig_ope == 1 or cig_ope == 2) and cig_len > max_indel) or
+                    (cig_ope == 4 or cig_ope == 5) and cig_len > max_clip):
+                    valid_flag = False
+                    break
+            if valid_flag:
+                fout.write(aln)
+
+    logger.info(f"Finished {log_func_str})")
+
+
+def filter_coverage_table(in_file, out_file, min_cov, min_mean_depth):
+    log_func_str = f'filter_coverage_table("{in_file}", "{out_file}", {min_cov}, {min_mean_depth})'
+    logger.info(f"Starting {log_func_str})")
+
+    df = (pl.read_csv(in_file, separator="\t")
+        .select("#rname", "coverage", "meandepth")
+        .filter((pl.col("coverage") >= min_cov) & (pl.col("meandepth") >= min_mean_depth))
+        .select("#rname")
+    )
+    df.write_csv(out_file, include_header=False)
+
+    logger.info(f"Finished {log_func_str})")
 
 
 #../test/bambu/filter/cmd.sh
