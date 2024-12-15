0acc03d403bb1e52b68b69d959d36eecfce810f9
rkajitani <rei.kajitani@gmail.com>
2024-12-15 23:03:45 +0000

    Update test.sh

--- isorefiner/test.sh
+++ isorefiner/test.sh
@@ -4,5 +4,11 @@ isorefiner trim -r ../test/reads.fastq.gz
 rm -rf isorefiner_filter_work
 isorefiner filter -i ../test/stringtie/out.gtf -r ../test/reads.fastq.gz -g ../test/genome_chr22.fa
 
+rm -r isorefiner_map_work
+isorefiner trim -r ../test/reads.fastq.gz -g ../test/genome_chr22.fa
+
+rm -rf isorefiner_stringtie_work
+isorefiner run_stringtie -b  ../test/map_genome/test.sorted.bam -g ../test/genome_chr22.fa -a ../test/annot_reduced.gtf
+
 rm -rf isorefiner_refine_work
 isorefiner refine -i ../test/*/filter/out_transcript.gtf -r ../test/reads.fastq.gz -g ../test/genome_chr22.fa -a ../test/annot_reduced.gtf
